c4e2c0dbea579ce426fda78ce398ec7a7a418837
git-minhyeong <rudolfh@knou.ac.kr>
2021-05-28 17:46:22 +0900

    session, with, Bs parse 예제

---
+++ beautifulSoup_Parsing.py
@@ -0,0 +1,5 @@
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(parseExampleHtml_doc, 'html.parser')
+
+print(soup.prettify())
---
+++ requests_session.py
@@ -0,0 +1,8 @@
+import requests
+
+s = requests.Session()
+s.headers.update({'user-agent' :
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"})
+
+r = s.get('http://naver.com')
+r = s.get('http://daum.net')
